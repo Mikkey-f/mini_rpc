c6685865f42701ec03af587f990af7b892ee3ed9
Mikkeyf <1647228132@qq.com>
2025-03-09 13:54:34 +0800

    加入RpcMessage来封装心跳请求

--- rpc/src/main/java/github/mikkeyf/remoting/transport/netty/server/NettyRpcServerHandler.java
+++ rpc/src/main/java/github/mikkeyf/remoting/transport/netty/server/NettyRpcServerHandler.java
@@ -1,5 +1,10 @@
 package github.mikkeyf.remoting.transport.netty.server;
 
+import github.mikkeyf.factory.SingletonFactory;
+import github.mikkeyf.handler.RpcRequestHandler;
+import github.mikkeyf.remoting.dto.RpcRequest;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -7,5 +12,16 @@ import lombok.extern.slf4j.Slf4j;
  * @CreateTime: 2025-03-09  13:35
  */
 @Slf4j
-public class NettyRpcServerHandler {
+public class NettyRpcServerHandler extends ChannelInboundHandlerAdapter {
+
+    private final RpcRequestHandler rpcRequestHandler;
+
+    public NettyRpcServerHandler() {
+        this.rpcRequestHandler = SingletonFactory.getSingleton(RpcRequestHandler.class);
+    }
+
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        if (msg instanceof ) {}
+    }
+
 }
